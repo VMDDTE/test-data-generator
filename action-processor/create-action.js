3a26e98d5c035478b5a14e1bc2076f09b0a5ee87
Scott Wilson-Billing <scott.wilsonbilling@mac.com>
2019-04-16 07:59:48 +0100

    Add create and delete product capability.

--- action-processor/create-action.js
+++ action-processor/create-action.js
@@ -4,6 +4,7 @@ const userTypes = require('../common/constants')
 const organisationService = require('../service/organisation-service')
 const localStorage = require('../service/local-storage-service')
 const userService = require('../service/user-service')
+const productService = require('../service/product-service')
 const log = global.log
 
 const SERVICE_NAME = 'create-action-processor'
@@ -18,6 +19,10 @@ async function process(namespace, action) {
         case actionTypes.TYPE_VET_PRACTICE_RECORD:
             log.info(`${SERVICE_NAME}::processing ${actionTypes.TYPE_VET_PRACTICE_RECORD}`)
             await createVetPractice(namespace, action)
+            break
+        case actionTypes.TYPE_PRODUCT:
+            log.info(`${SERVICE_NAME}::processing ${actionTypes.TYPE_PRODUCT}`)
+            await createProduct(namespace, action)
             break
         default:
             log.debug(`${SERVICE_NAME}::unrecognised action type ${action.type}`)
@@ -62,5 +67,25 @@ async function createVet(namespace, action) {
     vetIdList.push(responseData.Id)
     localStorage.setItem(namespace, 'vetIdList', vetIdList)
 }
+
+async function createProduct(namespace, action) {
+    log.debug(`${SERVICE_NAME}::createProduct`)
+    let productData = action.data
+    log.info(`${SERVICE_NAME}::createProduct::${action.label}::creating product from ${JSON.stringify(productData)}`)
+    let response = await productService.createProduct(productData)
+    let responseData = response.data
+    log.info(`${SERVICE_NAME}::createProduct::${action.label}::created:${JSON.stringify(responseData)}`)
+    var savedAction = localStorage.getItem(namespace, action.label)
+    savedAction.response = responseData
+    log.debug(`${SERVICE_NAME}::createProduct, saved action ${JSON.stringify(savedAction)}`)
+    localStorage.setItem(namespace, action.label, savedAction)
+    var productList = localStorage.getItem(namespace, 'productList')
+    if (!productList) {
+        productList = []
+    }
+    productList.push(responseData.ProductNo)
+    localStorage.setItem(namespace, 'productList', productList)
+}
+
 
 module.exports.process = process
