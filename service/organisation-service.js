62138d6cf0109e401a40b895654f86e29a33510b
Luis Miguel Gonzalez <luis@scs.uk.com>
2019-06-02 10:15:20 +0100

    Added npm modules for linter Set 4 spaces indentation lint settings Fixed linter errors Run linter so everything is indented with 4 spaces

--- service/organisation-service.js
+++ service/organisation-service.js
@@ -5,44 +5,43 @@ const ADMIN_BASE_API_URL = process.env.ADMIN_BASE_API_URL
 const SERVICE_NAME = 'organisation-service'
 
 async function createOrganisation (orgType, payload) {
-  let url = `${ADMIN_BASE_API_URL}/organisations/create/${orgType}`
-  log.info(`${SERVICE_NAME}::createOrganisation:url:${url}`)
+    let url = `${ADMIN_BASE_API_URL}/organisations/create/${orgType}`
+    log.info(`${SERVICE_NAME}::createOrganisation:url:${url}`)
 
-  return axios.post(url, payload)
-    .then((response) => {
-        return response.data
-    })
-    .catch(error => {
-        log.error(`${SERVICE_NAME}::createOrganisation:error: ${error}`)
-    })
+    return axios.post(url, payload)
+        .then((response) => {
+            return response.data
+        })
+        .catch(error => {
+            log.error(`${SERVICE_NAME}::createOrganisation:error: ${error}`)
+        })
 }
 
 async function updateOrganisation (payload) {
-  let url = `${ADMIN_BASE_API_URL}/organisations/updateVetPractice`
-  log.info(`${SERVICE_NAME}::updateOrganisation:url:${url}`)
+    let url = `${ADMIN_BASE_API_URL}/organisations/updateVetPractice`
+    log.info(`${SERVICE_NAME}::updateOrganisation:url:${url}`)
 
-  return axios.put(url, payload)
-    .then((response) => {
-        return response.data
-    })
-    .catch(error => {
-        log.error(`${SERVICE_NAME}::updateOrganisation:error: ${error}`)
-    })
+    return axios.put(url, payload)
+        .then((response) => {
+            return response.data
+        })
+        .catch(error => {
+            log.error(`${SERVICE_NAME}::updateOrganisation:error: ${error}`)
+        })
 }
 
 async function deleteOrganisation (orgId) {
-  let url = `${ADMIN_BASE_API_URL}/organisations/${orgId}`
-  log.info(`${SERVICE_NAME}::deleteOrganisation:url:${url}`)
+    let url = `${ADMIN_BASE_API_URL}/organisations/${orgId}`
+    log.info(`${SERVICE_NAME}::deleteOrganisation:url:${url}`)
 
-  return axios.delete(url)
-    .then((response) => {
-        return response.data
-    })
-    .catch(error => {
-        log.error(`${SERVICE_NAME}::deleteOrganisation:error: ${error}`)
-    })
+    return axios.delete(url)
+        .then((response) => {
+            return response.data
+        })
+        .catch(error => {
+            log.error(`${SERVICE_NAME}::deleteOrganisation:error: ${error}`)
+        })
 }
-
 
 module.exports.createOrganisation = createOrganisation
 module.exports.updateOrganisation = updateOrganisation
