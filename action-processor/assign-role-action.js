3f4bc1dd24305dbfd65473fa50591a467aee0d3d
Scott Wilson-Billing <scott.wilsonbilling@mac.com>
2019-07-02 17:45:32 +0100

    Add support for admin, primary admin and authorised user roles.

--- action-processor/assign-role-action.js
+++ action-processor/assign-role-action.js
@@ -49,6 +49,7 @@ async function createRole (namespace, action) {
     if (savedRoleProperties) {
         roleProperties = savedRoleProperties
     }
+    var roleName = ""
     switch (roleType) {
     case actionTypes.TYPE_VET_ROLE:
         if (roleProperties[roleNames.ROLE_NAME_VET]) {
@@ -56,38 +57,41 @@ async function createRole (namespace, action) {
         } else {
             roleProperties[roleNames.ROLE_NAME_VET] = userList
         }
+        roleName = roleNames.ROLE_NAME_VET
         break
     case actionTypes.TYPE_VET_PRIMARY_ADMIN_ROLE:
         if (roleProperties[roleNames.ROLE_NAME_VET_PRIMARY_ADMIN]) {
             roleProperties[roleNames.ROLE_NAME_VET_PRIMARY_ADMIN].push(...userList)
         }
         roleProperties[roleNames.ROLE_NAME_VET_PRIMARY_ADMIN] = userList
+        roleName = roleNames.ROLE_NAME_VET_PRIMARY_ADMIN
         break
     case actionTypes.TYPE_ADMIN_ROLE:
         if (roleProperties[roleNames.ROLE_NAME_ADMIN]) {
             roleProperties[roleNames.ROLE_NAME_ADMIN].push(...userList)
         }
         roleProperties[roleNames.ROLE_NAME_ADMIN] = userList
+        roleName = roleNames.ROLE_NAME_ADMIN
         break
     case actionTypes.TYPE_AUTHORISED_ROLE:
         if (roleProperties[roleNames.ROLE_NAME_AUTHORISED_USER]) {
             roleProperties[roleNames.ROLE_NAME_AUTHORISED_USER].push(...userList)
         }
         roleProperties[roleNames.ROLE_NAME_AUTHORISED_USER] = userList
+        roleName = roleNames.ROLE_NAME_AUTHORISED_USER
         break
     case actionTypes.TYPE_PRIMARY_ADMIN_ROLE:
         if (roleProperties[roleNames.ROLE_NAME_PRIMARY_ADMIN]) {
             roleProperties[roleNames.ROLE_NAME_PRIMARY_ADMIN].push(...userList)
         }
         roleProperties[roleNames.ROLE_NAME_PRIMARY_ADMIN] = userList
+        roleName = roleNames.ROLE_NAME_PRIMARY_ADMIN
         break
     }
     localStorage.setItem(namespace, roleLabel, roleProperties)
-    updatePayload['Id'] = orgId
-    updatePayload['Properties'] = roleProperties
-    log.debug(`${SERVICE_NAME}::createRole::properties ${JSON.stringify(updatePayload)}`)
-
-    await organisationService.updateOrganisation(updatePayload)
+    for (userId of userList) {
+        await organisationService.assignRoleToOrganisationForUser (orgId, userId, roleName)
+    }
 }
 
 module.exports.process = process
