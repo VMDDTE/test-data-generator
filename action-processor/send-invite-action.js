7061f8450bbcbb825aa65d94ad9e4c9c705657ea
Daniel Sutton <daniel@littlewolfstudio.co.uk>
2019-07-27 14:22:04 +0100

    added create invitation capability

---
+++ action-processor/send-invite-action.js
@@ -0,0 +1,67 @@
+const uuid = require('uuid')
+const actionTypes = require('../common/constants')
+const roleNames = require('../common/constants')
+const invitationService = require('../service/invitation-service')
+const localStorage = require('../service/local-storage-service')
+const log = global.log
+
+const SERVICE_NAME = 'send-invite-action-processor'
+
+async function process (namespace, action) {
+    log.debug(`${SERVICE_NAME}::${namespace}::process`)
+    switch (action.type) {
+    case actionTypes.ACTION_TYPE_PRIMARY_ADMIN_ROLE:
+    case actionTypes.ACTION_TYPE_ADMIN_ROLE:
+    case actionTypes.ACTION_TYPE_AUTHORISED_ROLE:
+        log.info(`${SERVICE_NAME}::processing ${action.type}`)
+        await sendInvitation(namespace, action)
+        break
+    default:
+        log.debug(`${SERVICE_NAME}::unrecognised action type ${action.type}`)
+        break
+    }
+}
+
+async function sendInvitation (namespace, action) {
+    let roleType = action.type
+    log.debug(`${SERVICE_NAME}::sendInvitation::type:${roleType}:${JSON.stringify(action)}`)
+    let roleData = action.data
+    let users = roleData.users
+    let orgIdLabel = roleData.orgId
+    let savedOrgAction = localStorage.getItem(namespace, orgIdLabel)
+    let response = savedOrgAction.response
+    let orgId = response.Id
+
+    var userList = []
+    for (const userLabel of users) {
+        let savedAction = await localStorage.getItem(namespace, userLabel)
+        let response = savedAction.response
+        let userId = response.Id
+        userList.push(userId)
+    }
+
+    var roleName = ""
+    switch (roleType) {
+    case actionTypes.ACTION_TYPE_ADMIN_ROLE:
+        roleName = roleNames.ROLE_NAME_ADMIN
+        break
+    case actionTypes.ACTION_TYPE_AUTHORISED_ROLE:
+        roleName = roleNames.ROLE_NAME_AUTHORISED_USER
+        break
+    case actionTypes.ACTION_TYPE_PRIMARY_ADMIN_ROLE:
+        roleName = roleNames.ROLE_NAME_PRIMARY_ADMIN
+        break
+    }
+
+    var savedInvitations = localStorage.getItem(namespace, 'invitations') || []
+
+    for (userId of userList) {
+        log.info(`${SERVICE_NAME}::createInvitation::about to send ${roleName} invitation to ${userId} for organisation with id ${orgId}`)
+        const invitation = await invitationService.createInvitation(userId, orgId, uuid.v1(), roleType)
+        savedInvitations.push(invitation.Id)
+    }
+
+    localStorage.setItem(namespace, 'invitations', savedInvitations)
+}
+
+module.exports.process = process
---
+++ service/invitation-service.js
@@ -0,0 +1,42 @@
+
+const axios = require('axios')
+const log = global.log
+const ADMIN_BASE_API_URL = process.env.ADMIN_BASE_API_URL
+const SERVICE_NAME = 'invitation-service'
+
+async function createInvitation (externalUserId, organisationId, reference, externalUserRole) {
+    let url = `${ADMIN_BASE_API_URL}/Invitation`
+
+    const payload = {
+        ExternalUserId: externalUserId,
+        OrganisationId: organisationId,
+        Reference: reference,
+        ExternalUserRole: externalUserRole
+    }
+
+    log.info(`${SERVICE_NAME}::createInvitation:url:${url}`)
+    return axios.post(url, payload)
+        .then((response) => {
+            return response.data
+        })
+        .catch(error => {
+            log.error(`${SERVICE_NAME}::createInvitation:error: ${error}`)
+            throw error
+        })
+}
+
+async function deleteInvitation (id) {
+    let url = `${ADMIN_BASE_API_URL}/Invitation/InvitationNo/${id}`
+    log.info(`${SERVICE_NAME}::deleteInvitation:url:${url}`)
+    return axios.delete(url)
+        .then((response) => {
+            return response.data
+        })
+        .catch(error => {
+            log.error(`${SERVICE_NAME}::deleteInvitation:error: ${error}`)
+            throw error
+        })
+}
+
+module.exports.createInvitation = createInvitation
+module.exports.deleteInvitation = deleteInvitation
