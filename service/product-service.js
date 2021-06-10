d538c92ab6743c9299ff24bf27f3ede35817406a
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2021-06-10 10:38:57 +0100

    DEV-16536 Updating products service to use specific service urls

--- service/product-service.js
+++ service/product-service.js
@@ -1,33 +1,34 @@
 
 const axios = require('axios')
 const log = global.log
-const TEST_SUPPORT_API_URL = process.env.TEST_SUPPORT_API_URL
 const SERVICE_NAME = 'product-service'
 
+const ADMIN_TEST_PRODUCTS_API_URL = process.env.ADMIN_API_URL + '/TestProducts'
+
 async function createProduct (payload) {
-    let url = `${TEST_SUPPORT_API_URL}/products`
-    log.info(`${SERVICE_NAME}::createProduct:url:${url}`)
+    let url = ADMIN_TEST_PRODUCTS_API_URL
+    log.info(`${SERVICE_NAME}::createProduct - url:${url}`)
 
     return axios.post(url, payload)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::createProduct:error: ${error}`)
+            log.error(`${SERVICE_NAME}::createProduct - error:${error}`)
             throw error
         })
 }
 
 async function deleteProduct (productNo) {
-    let url = `${TEST_SUPPORT_API_URL}/products/productNo/${productNo}`
-    log.info(`${SERVICE_NAME}::deleteProduct:url:${url}`)
+    let url = `${ADMIN_TEST_PRODUCTS_API_URL}/ByProductNo/${productNo}`
+    log.info(`${SERVICE_NAME}::deleteProduct - url:${url}`)
 
     return axios.delete(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::deleteProduct:error: ${error}`)
+            log.error(`${SERVICE_NAME}::deleteProduct - error:${error}`)
             throw error
         })
 }
