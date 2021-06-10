e7ad4439d6866f5645d8708fc8fdfc9ad32aa673
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2021-06-10 11:03:06 +0100

    DEV-16536 Updating substance qualifying service to use specific service urls

--- service/substance-qualifier-service.js
+++ service/substance-qualifier-service.js
@@ -3,30 +3,32 @@ const log = global.log
 const TEST_SUPPORT_API_URL = process.env.TEST_SUPPORT_API_URL
 const SERVICE_NAME = 'substance-qualifier-service'
 
+const ADMIN_TEST_REFERENCE_DATA_API_URL = process.env.ADMIN_API_URL + '/TestReferenceData'
+
 async function createSubstanceQualifier (payload) {
-    let url = `${TEST_SUPPORT_API_URL}/reference/substance`
-    log.info(`${SERVICE_NAME}::createSubstanceQualifier:url:${url}`)
+    let url = `${ADMIN_TEST_REFERENCE_DATA_API_URL}/Substance`
+    log.info(`${SERVICE_NAME}::createSubstanceQualifier - url:${url}`)
 
     return axios.post(url, payload)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::createSubstanceQualifier:error: ${error}`)
+            log.error(`${SERVICE_NAME}::createSubstanceQualifier - error:${error}`)
             throw error
         })
 }
 
 async function deleteSubstanceQualifier (substanceId) {
-    let url = `${TEST_SUPPORT_API_URL}/reference/substance/${substanceId}`
-    log.info(`${SERVICE_NAME}::deleteSubstanceQualifier:url:${url}`)
+    let url = `${ADMIN_TEST_REFERENCE_DATA_API_URL}/Substance/${substanceId}`
+    log.info(`${SERVICE_NAME}::deleteSubstanceQualifier - url:${url}`)
 
     return axios.delete(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::deleteSubstanceQualifier:error: ${error}`)
+            log.error(`${SERVICE_NAME}::deleteSubstanceQualifier - error:${error}`)
             throw error
         })
 }
