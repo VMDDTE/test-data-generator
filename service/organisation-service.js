82e95dea8ad9d9ab850d761f42f175b6f348e62c
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2021-06-10 10:36:07 +0100

    DEV-16536 Updating organisation service to use specific service urls

--- service/organisation-service.js
+++ service/organisation-service.js
@@ -1,12 +1,12 @@
-
 const axios = require('axios')
 const log = global.log
-const TEST_SUPPORT_API_URL = process.env.TEST_SUPPORT_API_URL // This is TestSupport
 const SERVICE_NAME = 'organisation-service'
 
+const ORG_ADMIN_TEST_ORGANISATIONS_API_URL = process.env.ORG_ADMIN_API_URL + '/TestOrganisations'
+
 async function createOrganisation (orgType, payload) {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/create/${orgType}`
-    log.info(`${SERVICE_NAME}::createOrganisation:url:${url}`)
+    let url = `${ORG_ADMIN_TEST_ORGANISATIONS_API_URL}/Create/${orgType}`
+    log.info(`${SERVICE_NAME}::createOrganisation - url:${url}`)
 
     return axios.post(url, payload)
         .then(async (response) => {
@@ -14,29 +14,14 @@ async function createOrganisation (orgType, payload) {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::createOrganisation:error: ${error}`)
-            throw error
-        })
-}
-
-async function updateOrganisation (payload) {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/updateVetPractice`
-    log.info(`${SERVICE_NAME}::updateOrganisation:url:${url}`)
-
-    return axios.put(url, payload)
-        .then(async (response) => {
-            await rebuildRedisOrganisationsCache()
-            return response.data
-        })
-        .catch(error => {
-            log.error(`${SERVICE_NAME}::updateOrganisation:error: ${error}`)
+            log.error(`${SERVICE_NAME}::createOrganisation - error:${error}`)
             throw error
         })
 }
 
 async function deleteOrganisation (orgId) {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/${orgId}`
-    log.info(`${SERVICE_NAME}::deleteOrganisation:url:${url}`)
+    let url = `${ORG_ADMIN_TEST_ORGANISATIONS_API_URL}/${orgId}`
+    log.info(`${SERVICE_NAME}::deleteOrganisation - url:${url}`)
 
     return axios.delete(url)
         .then(async (response) => {
@@ -44,54 +29,53 @@ async function deleteOrganisation (orgId) {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::deleteOrganisation:error: ${error}`)
+            log.error(`${SERVICE_NAME}::deleteOrganisation - error:${error}`)
             throw error
         })
 }
 
 async function rebuildRedisOrganisationsCache () {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/RebuildCache`
-    log.info(`${SERVICE_NAME}::rebuildRedisOrganisationsCache:url:${url}`)
+    let url = `${ORG_ADMIN_TEST_ORGANISATIONS_API_URL}/RebuildCache`
+    log.info(`${SERVICE_NAME}::rebuildRedisOrganisationsCache - url:${url}`)
     return axios.put(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::rebuildRedisOrganisationsCache:error: ${error}`)
+            log.error(`${SERVICE_NAME}::rebuildRedisOrganisationsCache - error:${error}`)
             throw error
         })
 }
 
 async function findOrganisationByName (organisationType, name) {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/byTypeAndName/${organisationType}/${name}`
-    log.info(`${SERVICE_NAME}::findOrganisationByName:url:${url}`)
+    let url = `${ORG_ADMIN_TEST_ORGANISATIONS_API_URL}/${organisationType}/${name}`
+    log.info(`${SERVICE_NAME}::findOrganisationByName - url:${url}`)
 
     return axios.get(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::findOrganisationByName:error: ${error}`)
+            log.error(`${SERVICE_NAME}::findOrganisationByName - error:${error}`)
             throw error
         })
 }
 
 async function assignRoleToOrganisationForUser (orgId, userId, roleName) {
-    let url = `${TEST_SUPPORT_API_URL}/organisations/${orgId}/assignrole/${userId}/${roleName}`
-    log.info(`${SERVICE_NAME}::assignRoleToOrganisationForUser:url:${url}`)
+    let url = `${ORG_ADMIN_TEST_ORGANISATIONS_API_URL}/${orgId}/assignrole/${userId}/${roleName}`
+    log.info(`${SERVICE_NAME}::assignRoleToOrganisationForUser - url:${url}`)
 
     return axios.put(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::assignRoleToOrganisationForUser:error: ${error}`)
+            log.error(`${SERVICE_NAME}::assignRoleToOrganisationForUser - error:${error}`)
             throw error
         })
 }{}
 
 module.exports.createOrganisation = createOrganisation
-module.exports.updateOrganisation = updateOrganisation
 module.exports.deleteOrganisation = deleteOrganisation
 module.exports.assignRoleToOrganisationForUser = assignRoleToOrganisationForUser
 module.exports.findOrganisationByName = findOrganisationByName
