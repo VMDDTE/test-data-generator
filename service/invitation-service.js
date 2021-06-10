9676694f93a9f4397eac13a51227e7e695dcdb14
Simon Ellett <simon.ellett@technocloud-software.co.uk>
2021-06-10 09:47:39 +0100

    DEV-16536 Reworking invitations to use direct service url and adding logic as found in TestSupport calls mapping property/ignoring certain error status codes

--- service/invitation-service.js
+++ service/invitation-service.js
@@ -1,40 +1,45 @@
-
 const axios = require('axios')
 const log = global.log
-const TEST_SUPPORT_API_URL = process.env.TEST_SUPPORT_API_URL
 const SERVICE_NAME = 'invitation-service'
 
-async function createInvitation (externalUserId, organisationId, reference, externalUserRole) {
-    let url = `${TEST_SUPPORT_API_URL}/Invitation`
+const ORG_ADMIN_TEST_INVITATIONS_API_URL = process.env.ORG_ADMIN_API_URL + '/TestInvitations'
+const ORG_ADMIN_INVITATIONS_API_URL = process.env.ORG_ADMIN_API_URL + '/Invitations'
 
+async function createInvitation (externalUserId, organisationId, reference, externalUserRole) {
+    let url = ORG_ADMIN_TEST_INVITATIONS_API_URL
+
+    // Not sure when ExternalUserRole changed to InviteeRole, but TestSupport was doing this mapping
     const payload = {
         ExternalUserId: externalUserId,
         OrganisationId: organisationId,
         Reference: reference,
-        ExternalUserRole: externalUserRole
+        InviteeRole: externalUserRole 
     }
 
-    log.info(`${SERVICE_NAME}::createInvitation:url:${url}`)
+    log.info(`${SERVICE_NAME}::createInvitation - url:${url}`)
     return axios.post(url, payload)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::createInvitation:error: ${error}`)
+            log.error(`${SERVICE_NAME}::createInvitation - error: ${error}`)
             throw error
         })
 }
 
 async function deleteInvitation (id) {
-    let url = `${TEST_SUPPORT_API_URL}/Invitation/InvitationNo/${id}`
-    log.info(`${SERVICE_NAME}::deleteInvitation:url:${url}`)
+    let url = `${ORG_ADMIN_INVITATIONS_API_URL}/${id}`
+    log.info(`${SERVICE_NAME}::deleteInvitation - url:${url}`)
     return axios.delete(url)
         .then((response) => {
             return response.data
         })
         .catch(error => {
-            log.error(`${SERVICE_NAME}::deleteInvitation:error: ${error}`)
-            throw error
+            // TestSupport had logic to ignore BadRequest/NotFound
+            if(error.response.status !== 400 && error.response.status !== 404){
+                log.error(`${SERVICE_NAME}::deleteInvitation - error: ${error}`)
+                throw error
+            }
         })
 }
 
